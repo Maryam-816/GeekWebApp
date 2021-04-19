43fdfd50be3c355584b7d24f2d5d532d6e937bfb
Maryam-816 <rustamova.maryam2009@gmail.com>
2021-04-19 14:14:42 +0400

    Migration added

---
+++ GeekWebAppProject/Migrations/202104171325083_Initial.cs
@@ -0,0 +1,96 @@
+﻿namespace GeekWebAppProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Articles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 20),
+                        Text = c.String(nullable: false, maxLength: 200),
+                        ImagePath = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Feedbacks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ImagePath = c.String(),
+                        Text = c.String(nullable: false, maxLength: 200),
+                        ClientFullName = c.String(nullable: false, maxLength: 100),
+                        ClientJob = c.String(nullable: false, maxLength: 60),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Footers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Text = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Menus",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LogoImagePath = c.String(),
+                        Name = c.String(nullable: false, maxLength: 60),
+                        Text = c.String(nullable: false, maxLength: 50),
+                        NavName = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.SkillsModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Procent = c.Int(nullable: false),
+                        Language = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ValuableClients",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ImagePath = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.WorkModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ImagePath = c.String(),
+                        Text = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.WorkModels");
+            DropTable("dbo.ValuableClients");
+            DropTable("dbo.SkillsModels");
+            DropTable("dbo.Menus");
+            DropTable("dbo.Footers");
+            DropTable("dbo.Feedbacks");
+            DropTable("dbo.Articles");
+        }
+    }
+}
---
+++ GeekWebAppProject/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace GeekWebAppProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Data.GeekDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Data.GeekDbContext context)
+        {
+            
+        }
+    }
+}
