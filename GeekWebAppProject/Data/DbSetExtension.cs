6e4b94254ae21d2cdfb31ac6dd719151d4664cee
Maryam-816 <rustamova.maryam2009@gmail.com>
2021-04-19 14:16:15 +0400

    ContactsModel added DbSetExtension added User model added

---
+++ GeekWebAppProject/Data/DbSetExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeekWebAppProject.Data
+{
+    public static class DbSetExtension
+    {
+        public static User GetUser(this DbSet<User> _users, ContactsModel contactsModel)
+        {
+            if (!_users.Any(q => q.Email == contactsModel.Email))
+            {
+                return null;
+            }
+
+            else
+            {
+                return (User)_users.Where(q => q.Email == contactsModel.Email);
+            }
+        }
+    }
+}
---
+++ GeekWebAppProject/Data/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeekWebAppProject.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 55, MinimumLength = 5)]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(maximumLength: 15, MinimumLength = 5)]
+        public string Password { get; set; }
+
+    }
+}
