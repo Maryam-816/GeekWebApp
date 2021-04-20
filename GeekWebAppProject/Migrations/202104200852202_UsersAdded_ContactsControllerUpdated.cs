8d1b06c56a717a38ed49c9b3f2201ebf3582623d
Maryam-816 <rustamova.maryam2009@gmail.com>
2021-04-20 12:53:33 +0400

    Users migration added

---
+++ GeekWebAppProject/Migrations/202104200852202_UsersAdded_ContactsControllerUpdated.cs
@@ -0,0 +1,28 @@
+﻿namespace GeekWebAppProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UsersAdded_ContactsControllerUpdated : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(nullable: false, maxLength: 55),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false, maxLength: 15),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
