cac08f761c847bfa3558516d41f76ebd9b41eef5
Maryam-816 <rustamova.maryam2009@gmail.com>
2021-04-21 16:34:23 +0400

    new migration added

---
+++ GeekWebAppProject/Migrations/202104200955371_ContactsModelsRenamedToContactMessages.cs
@@ -0,0 +1,18 @@
+﻿namespace GeekWebAppProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ContactsModelsRenamedToContactMessages : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.ContactsModels", newName: "ContactMessages");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.ContactMessages", newName: "ContactsModels");
+        }
+    }
+}
