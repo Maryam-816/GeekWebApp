a1f753da072b2f7466bea92ae378b6424d3b6532
Maryam-816 <rustamova.maryam2009@gmail.com>
2021-05-01 23:45:39 +0400

    new ContactInfo migration added

---
+++ GeekWebAppProject/Migrations/202105011927433_ContactInfosAdded.cs
@@ -0,0 +1,27 @@
+﻿namespace GeekWebAppProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ContactInfosAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ContactInfoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Info = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ContactInfoes");
+        }
+    }
+}
